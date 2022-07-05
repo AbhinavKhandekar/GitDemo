6233072225b5f81f76f2818b5ec4cb6b79fc0d94
Abhinav <abhikhandekar66@gmail.com>
2022-07-05 08:18:41 -0700

    My First Commit

---
+++ src/test/java/stepDefinations/O_2.java
@@ -0,0 +1,29 @@
+package stepDefinations;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class O_2 {
+
+
+
+	@RunWith(Cucumber.class)
+	public class MyStepDefinitions {
+
+	    @Given("^user is on OrangeHrm landing page$")
+	    public void user_is_on_orangehrm_landing_page() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @When("^user enter valid user name and passward$")
+	    public void user_enter_valid_user_name_and_passward() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @Then("^user should logged in sucessfully$")
+	    public void user_should_logged_in_sucessfully() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	}
+}
---
+++ target/test-classes/features/Orangehrm_login.feature
@@ -0,0 +1,10 @@
+Feature: Open Orange Hrm and user login with valid creadential
+Scenario: Login experince with valid id and Passward
+Given user is on OrangeHrm landing page
+When user enter valid user name and passward
+Then user should logged in sucessfully
+ 
+ 
+ 
+ 
+ 
